e1086f34823a662bc16f9ee57f4f4b4dedcbc333
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-10-13 10:04:15 +0100

    Create Game.sol

---
+++ Contracts/Game.sol
@@ -0,0 +1,10 @@
+// @title GAME token for battledogs Arena
+// https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
+
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "abdk-libraries-solidity/ABDKMath64x64.sol";
+import "./ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract GAME is ERC20, Ownable, ReentrancyGuard { }
